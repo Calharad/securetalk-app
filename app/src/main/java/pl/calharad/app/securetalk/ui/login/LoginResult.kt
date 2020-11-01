76e7bf9a14a4106cd82aabf793b9ad8b8bced9b8
Calharad <adudek0610@gmail.com>
2020-11-01 21:34:52 +0100

    project init

---
+++ app/src/main/java/pl/calharad/app/securetalk/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package pl.calharad.app.securetalk.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
